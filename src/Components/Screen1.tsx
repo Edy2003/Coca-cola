8449aa213fe8f2f5b7930d78968c42283d5fe8c1
edikk <edikkopcha6@gmail.com>
2023-08-10 17:08:34 +0300

    Added burger menu animation

--- src/Components/Screen1.tsx
+++ src/Components/Screen1.tsx
@@ -1,10 +1,14 @@
 import '../Styles/screen1.css'
 import WebFont from 'webfontloader';
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function ScreenOne (){
+    const [active,setActive]=useState<boolean>(false)
 
+    function activation(){
+        setActive(!active)
+    }
     useEffect(() => {
         WebFont.load({
             google: {
@@ -19,13 +23,18 @@ function ScreenOne (){
                 <div className='first-screen-top-bar'>
                     <div className='first-screen-top-bar-logo'></div>
                     <div className='first-screen-top-bar-buttons'>
-                        <div className='first-screen-top-bar-menu'>
+                        <div className={active? 'activated-menu':'first-screen-top-bar-menu'}>
                             <a className='top-bar-menu-links'>Main</a>
                             <a className='top-bar-menu-links'>About</a>
                             <a className='top-bar-menu-links'>Products</a>
                             <a className='top-bar-menu-links'>Contacts</a>
                         </div>
-                        <div className='first-screen-top-bar-burger'></div>
+                        <div className={'first-screen-top-bar-burger'}onClick={activation}></div>
+                        <div className={active? 'activated-burger':'first-screen-top-bar-burger-animation' }onClick={activation}>
+                            <div className={active? 'activated-burger-lines':'lines'}></div>
+                            <div className={active? 'activated-burger-lines':'lines'}></div>
+                            <div className={active? 'activated-burger-lines':'lines'}></div>
+                        </div>
                     </div>
                 </div>
 
